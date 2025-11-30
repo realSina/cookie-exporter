c4b6a8b194cba3557e4a6d3751de5284d49206a5
SYRE <51854509+realSina@users.noreply.github.com>
2025-11-30 15:20:58 +0330

    Create saver.js

---
+++ saver.js
@@ -0,0 +1,7 @@
+function saveAs(blob, filename) {
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(blob);
+    link.download = filename;
+    link.click();
+    URL.revokeObjectURL(link.href);
+}
